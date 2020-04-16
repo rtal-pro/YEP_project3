c6a13818a0c6c9bcc5681cef0ed7ff68a27f3219
Charles Robertson <charles.robertson@epitech.eu>
2020-04-16 14:59:50 +0200

    [+] New configuration for mobile app

---
+++ mobile/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  parser: 'babel-eslint',
+  extends: 'airbnb',
+  plugins: ['react', 'jsx-a11y', 'import', 'react-hooks'],
+  rules: {
+    'react/jsx-filename-extension': [1, {extensions: ['.js', '.jsx']}],
+  },
+};
